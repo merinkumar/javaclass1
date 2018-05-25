9fc75e1bfbe7f89f500436a608ac45deeffdc445
merin_83 <talktomerin@gmail.com>
2018-05-26 00:03:35 +0530

    initial commit

--- src/com/merin/Main.java
+++ src/com/merin/Main.java
@@ -4,6 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        float a = 10,b = 7;
+                System.out.println("Hello World");
+        Class1 object1 = new Class1();
+        object1.method1("Merin");
+        boolean result = object1.method2(args);
+        if (result == true){
+            System.out.println("Argument present");
+        }
+        else {
+            System.out.println("No Argument present");
+        }
+
+        extClass math = new extClass();
+
+        System.out.println("Sum of " + a + " + " + b + " is " + math.add(a,b));
+        System.out.println("Multiply " + a + " by " + b + " is " + math.mull(b,a));
+
     }
+}
+
+class Class1 {
+    void method1 (String s1){
+        System.out.println("Hello " + s1 + ", Welcome !!");
+    }
+
+    boolean method2 (String[] args){
+        boolean result = false;
+        if (args.length > 0){
+            return true;
+        }
+
+        return false;
+    }
+
 }
---
+++ src/com/merin/extClass.java
@@ -0,0 +1,15 @@
+package com.merin;
+
+public class extClass {
+
+    public double f3;
+    public double add (float n1, float n2){
+        return n1 + n2;
+    }
+
+    public  double mull (float f1, float f2){
+        f3 = (f1 * f2);
+        return f3;
+
+    }
+}
